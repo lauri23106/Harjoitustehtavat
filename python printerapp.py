9fb8fa242935facda3ae767b076166edfde98254
lauri23106 <146861978+lauri23106@users.noreply.github.com>
2023-11-04 18:10:32 +0200

    Tehtiin python printer app

---
+++ python printerapp.py
@@ -0,0 +1,38 @@
+# 1. Declare variables
+creator = "Lauri"
+number1 = 5
+number2 = 2
+multiplication = number1 * number2
+addition = number1 + number2
+division = number1 / number2
+subtraction = number1 - number2
+
+# 2. Print "Printer App"
+print("Printer App")
+
+# 3. Print "This app is made by:"
+print("This app is made by:")
+
+# 4. Print the value of the creator
+print("This app is made by:", creator)
+
+# 5. Print the value of number1
+print("The value of number1 is", number1)
+
+# 8. Print "The value of number1 is 5"
+print("The value of number1 is", number1)
+
+# 9. Print "The value of number2 is 3"
+print("The value of number2 is", number2)
+
+# 10. Print multiplication
+print(f"{number1} * {number2} = {multiplication}")
+
+# 11. Print addition
+print(f"{number1} + {number2} = {addition}")
+
+# 12. Print division
+print(f"{number1} / {number2} = {division}")
+
+# 13. Print subtraction
+print(f"{number1} - {number2} = {subtraction}")
